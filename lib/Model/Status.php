713ee9b5ff62e6a7bbb20391e199276b2a1d8a53
dantleech <dan.t.leech@gmail.com>
2019-01-21 21:37:43 +0100

    integrates URL store

--- lib/Model/Status.php
+++ lib/Model/Status.php
@@ -29,6 +29,16 @@ class Status
      */
     public $queueSize = 0;
 
+    /**
+     * @var CircularUrlStore
+     */
+    public $urlStore;
+
+    public function __construct(CircularUrlStore $urlStore)
+    {
+        $this->urlStore = $urlStore;
+    }
+
     public function failurePercentage(): float
     {
         if ($this->nbFailures === 0) {
