28169baf00da93079a2d4acf12321ad992959f6f
Daniel Leech <dan.t.leech@gmail.com>
2020-04-01 21:59:25 +0100

    Add composer `integrate` script
    To run the tests etc

--- tests/Example/custom-headers/teapot.php
+++ tests/Example/custom-headers/teapot.php
@@ -1,7 +1,6 @@
 <?php
 
 $headers = getallheaders();
-file_put_contents('headers', var_export($headers,true));
 
 if ($headers['X-One'] === 'Teapot' && $headers['X-Two'] === 'Pottea') {
     http_response_code(418);
